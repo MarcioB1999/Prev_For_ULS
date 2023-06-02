a84af550a05e4d130674b818ecef40946fd82950
Marcio <marciobarros.2310@gmail.com>
2023-06-02 01:37:50 -0300

    ...

--- Gerador_demandas/criar_demandas.py
+++ Gerador_demandas/criar_demandas.py
@@ -19,8 +19,9 @@ if __name__=="__main__":
 
     #funcao_demanda = lambda i: 10000+5*np.log(i+6)+20*np.cos(6.2831*i/50+1.8849)+np.random.normal(0, 8,1)[0]
     demandas = []
+
     for indice in range(int(K/52)):
-        funcao_demanda = lambda i: 10000+400*np.cos(6.2831*(i+10)/50+1.8849)+np.random.normal(0, 30,1)[0]
+        funcao_demanda = lambda i: int(10000+400*np.cos(6.2831*(i+10)/50+1.8849)+np.random.normal(0, 30,1)[0])
         demandas += [funcao_demanda(i) for i in range(52)]
     
     df_demandas = pd.DataFrame({'date':date,'demandas':demandas})
